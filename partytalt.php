29b1a03e178ce46b8027a2dccfdaa57c6990815d
EddieGustafsson <eddie.gustafsson@elev.ga.ntig.se>
2019-09-20 09:19:26 +0200

    Tog bort onödiga else funktioner.

--- partytalt.php
+++ partytalt.php
@@ -3,12 +3,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Git out of here</title>
-            <meta name="description" content="">
+    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
 </head>
 <body>
-<img src="bilder/espresso.jpg">
+
 <?php
 try {
 
@@ -16,31 +15,21 @@ try {
     $apple_ar_frukt = false;
 
     if ($apple_ar_frukt==true){
-        
-         echo 'äpplen är frukt';
-        
-    }
-    else{
-        echo 'äpplen är slavar';
+        echo 'äpplen är frukt';   
     }
     if($banan_ar_frukt == true){
         echo 'banan är frukt';
     }
-    else{
-        echo 'banan är bär';
-    }
+
     
 }catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-?>
-</body>
-</html>
-<?php
-error_log('nu har vi kört denna X antal gånger',3,'error.log');
 /*
 Git out of here!
 Syftet med denna hemsida är att visa om en banan eller äpple är en frukt
 (Kan det bli bättre än ovan????)
 */
 ?>
+</body>
+</html>
