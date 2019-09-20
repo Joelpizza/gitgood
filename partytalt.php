8bd618710fce6ea4dedf2e2fa95eab7273b56ec3
Nils Pettersson <nils.pettersson2@learnet.se>
2019-09-20 10:53:12 +0200

    added slavesgit add .

---
+++ partytalt.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>är bananer och äpplen frukt?</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php
+try {
+
+    $banan_ar_frukt = true;
+    $apple_ar_frukt = true;
+
+    if ($apple_ar_frukt==true){
+        echo '<img src="bilder/apple.jpg" alt="Äpple">';
+        echo 'äpplen är frukt <br>';
+           
+    }
+    if($banan_ar_frukt == true){
+        echo '<img src="bilder/banan.jpg" alt="Banan">';
+        echo 'banan är frukt';
+    }
+
+}catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+?>
+</body>
+</html>
+
