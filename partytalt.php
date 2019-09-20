67077024f83c83f801c18c1ee0db6e48c8c88d58
Joel Magnusson <joel.magnusso@elev.ga.ntig.se>
2019-09-20 10:40:43 +0200

    k

--- partytalt.php
+++ partytalt.php
@@ -3,13 +3,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>är bananer och äpplen frukt?</title>
-    <meta name="description" content="">
+    <meta name="description" content="bananer">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
 <?php
-try {
+
 
     $banan_ar_frukt = true;
     $apple_ar_frukt = true;
@@ -23,9 +23,7 @@ try {
     }
 
     
-}catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
-}
+
 
 ?>
 </body>
